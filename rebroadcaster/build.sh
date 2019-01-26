78ed46720bb26ccd9eae6bb609a87b8589ce264f
mango <petershallue@hotmail.com>
2019-01-26 05:03:17 +0000

    First Commit

--- rebroadcaster/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-SECONDS=0
-. ../common.sh
-
-docker_build rebroadcaster
-echo "Built in ${SECONDS} seconds"
-
--- rebroadcaster/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Start server rebroadcaster
-HOSTNAME="A Minecraft Server"
-[[ -z "$HOSTNAME" ]] && HOSTNAME="Dick's 4 Life"
-
-python3 advertise.py "$HOSTNAME" 
-
-
